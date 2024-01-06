7a2642ee93f741ea51fb4f494d6a3af60290d018
pdrodavi <pedrodaviti@gmail.com>
2024-01-06 06:52:54 -0300

    v1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 "# macrocalc" 
+https://nutrium.com/blog/pt-br/equacao-de-mifflin-st-jeor-para-profissionais-de-nutricao/
+
+https://blog.dietbox.me/como-calcular-o-gasto-energetico/
--- assets/js/script.js
+++ assets/js/script.js
@@ -12,56 +12,62 @@ function handleSubmit (event) {
   const activityLevel = getSelectedValue('activity_level');
   const target = getSelectedValue('target');
 
+  /*
   const tmb = Math.round(
     gender === 'female'
-    ? (655 + (9.6 * weight) + (1.8 * height) - (4.7 *age)) //if (cálculo pronto)
-    : (66 + (13.7 * weight) + (5 * height) - (6.8 *age)) //else
-  );
+    ? (655 + (9.6 * weight) + (1.9 * height) - (4.7 *age)) //if (cálculo pronto)
+    : (66 + (13.8 * weight) + (5 * height) - (6.8 *age)) //else
+  );*/
+
+  const tmb = calcMifflin(weight, height, age, gender);
 
   const maintenance = Math.round(tmb * Number(activityLevel));
 
   const loseWeight = target === 'cutting' ? maintenance - 450 : maintenance + 450;
 
+  const targetTitle = target === 'cutting' ? 'Lose Fat' : 'Lean Mass Gain';
+  const targetSubTitle = target === 'cutting' ? 'DCA / CAD: ' : 'GCA / CAG: ';
   //const loseWeight = maintenance - 450;
   //const gainWeight = maintenance + 450;
 
   const carbo = Math.round((loseWeight * 0.60) / 4);
   const protein = Math.round((loseWeight * 0.20) / 4);
   const gordura = Math.round((loseWeight * 0.20) / 9);
-
-  /*
-  console.log(gender)
-  console.log(age)
-  console.log(weight)
-  console.log(height)
-  console.log(activityLevel)
-  console.log(target)
-  console.log(tmb)
-  console.log(maintenance)
-  console.log(loseWeight)
-  console.log(carbo)
-  console.log(protein)
-  console.log(gordura)
-  */
+  const imc = calcIMC(weight, height);
 
   //resultados da cálculo para exibir na tela
   const layout = `
-    <h4>Macronutrientes por dia</h4>
+    <h4>${targetTitle}</h4>
     <br>
     <div class="result-content">
       <ul>
+      <br><br>
+      <h5>Your indexes</h5>
+      <br>
+      <li>
+          IMC / BMI: <strong>${imc} </strong>
+        </li>
+        <li>
+        ENA / ERB: <strong>${maintenance} </strong>kcal<br>
+        </li>
+        <li>
+          ${targetSubTitle} <strong>450</strong> kcal
+        </li>
+        <li>
+          END / ERA: <strong>${loseWeight} </strong>kcal<br>
+        </li>
+        <li>
+          TMB / BMR: <strong>${tmb} </strong>
+        </li>
+      <br><br>
+      <h5>Macronutrients per day</h5>
       <br>
         <li>
-          Carboidratos: <strong> ${carbo} </strong>g<br>
-          Proteínas: <strong> ${protein} </strong>g<br>
-          Gorduras: <strong> ${gordura} </strong>g<br>
+          Carbohydrates: <strong> ${carbo} </strong>g<br>
+          Proteins: <strong> ${protein} </strong>g<br>
+          Lipids: <strong> ${gordura} </strong>g<br>
         </li>
-        <li><br>
-          Consumo médio: <strong>${loseWeight} </strong>kcal por dia<br>
-        </li>
-        <li>
-          Seu metabolismo basal é de <strong>${tmb} </strong>kcal
-        </li>
+        
         <br>
       </ul>
   `;
@@ -79,4 +85,26 @@ function getSelectedValue(id) {
 
 function getInputNumberValue(id) {
   return Number(document.getElementById(id).value);
+}
+
+const calcBenedict = (weight, size, age, gender) => {
+    if (gender === "male"){
+        return Math.round(66.5+(13.75*weight)+(5*size)-(6.78*age));
+    }
+    else{
+        return Math.round(655+(9.6*weight)+(1.85*size)-(4.68*age));
+    }
+}
+
+const calcMifflin = (weight, height, age, gender) => {
+    if (gender === "male"){
+        return Math.round((10*weight) + (6.25*height) - (5*age) + 5);
+    }
+    else{
+        return Math.round((10*weight) + (6.25*height) - (5*age) - 161);
+    }
+}
+
+const calcIMC = (weight, size) => {
+  return (weight / ((size / 100) * (size / 100))).toFixed(2);
 }
